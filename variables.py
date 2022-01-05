62476ce57a2477a5dffbda7379558f0ecd79766b
Ashish Rajesh Lulla <65806448+Ashishlulla@users.noreply.github.com>
2022-01-05 19:24:49 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,102 @@
+# Creating Variables
+
+# a = "abc"
+# print(a)
+# b = 12
+# print(b)
+# c = 2.1
+# print(c)
+
+# Getting Type of Variable
+
+# a = "abc"
+# print(type(a))
+# b = 12
+# print(type(b))
+# c = 2.1
+# print(type(c))
+
+# Type Casting in Variables
+
+# Casting Value to String:
+
+# a =str("abc")
+# print(type(a))
+# b =str(1)
+# print(type(b))
+# c = str(2.1)
+# print(type(c))
+
+
+# Casting Value to Integer:
+
+# a = int("abc")
+# print(type(a))
+# b = int(1)
+# print(type(b))
+# c = int(2.1)
+# print(type(c))
+
+# Casting Value to Float:
+
+# a =float("abc")
+# print(type(a))
+# b =float(1)
+# print(type(b))
+# c = float(2.1)
+# print(type(c))
+
+# ------------------ Case Sensitivity----------------
+
+# age = 20
+# Age = 21
+# AGE = 22
+# print(age)
+# print(Age)
+# print(AGE)
+
+# ----------------- Rules for Variables Name -------------------------
+
+"""
+    1. Variable name should not start with integer Value.
+    2. Variable name should not contain any special character or Arithmetic
+    3. Variable name should not separated by space 
+    Examples:
+"""
+# Invalid Variables Name:
+# 1a = 20
+# a@b = 20
+# a b = 20
+
+# Valid  Variables Name:
+
+# a_b = 20
+# ab = 20
+# _ab = 20
+# a_b_2 = 20
+# AB = 20
+
+# ----------------assigning single value to multiple variable -------------------
+
+# a = b = c = 10
+# print(a)
+# print(b)
+# print(c)
+
+# ----------------assigning multiple value to multiple variable -------------------
+
+# a, b, c = 1, 2, 3
+# print(a)
+# print(b)
+# print(c)
+
+
+# ----------------------- EXERCISES ---------------------
+
+# 1. Create a variable and assign "Hello World" and print the variable
+# 2. Create a two variable and assign integer value to them
+# and print their SUM
+# 3. 2my-first_name = "John" correct the variable name and print "John"
+# 4. Assign the same value to all three variables in one single code line.
+# 5. Assign the three integer values to all three variables and print their sum
+#    in one single code line.
