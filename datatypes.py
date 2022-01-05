9025b922aa26f32f17e9f92ff29941c3544603ec
Ashish Rajesh Lulla <65806448+Ashishlulla@users.noreply.github.com>
2022-01-05 19:25:38 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,102 @@
+# String Data Type
+# x = "Hello World"
+# print(x)
+# print(type(x))
+#
+# # Integer data Type
+# x = 20
+# print(x)
+# print(type(x))
+#
+# # Float Data Type
+# x = 20.5
+# print(x)
+# print(type(x))
+
+# Complex Data Type
+# x = 1j
+# print(x)
+# print(type(x))
+
+# # List Data Type
+# x = ["apple", "banana", "cherry"]
+# print(x)
+# print(type(x))
+#
+#
+# # Tuple Data Type
+# # x = ("apple", "banana", "cherry")
+# # print(x)
+# # print(type(x))
+#
+# # Range Data Type
+# x = range(200, 6)
+# print(x)
+# print(type(x))
+
+# Dictionary data Type
+# x = {"name" : "John", "age" : 36}
+# print(x)
+# print(type(x))
+
+# Set Data Type
+# x = {"apple", "banana", "cherry"}
+# print(x)
+# print(type(x))
+
+# Boolean Data Type (True or False)
+
+# print(type(1))
+# print(type(0))
+
+# -------------------------- Set Datatype ---------------------------
+
+# Setting value to string:
+# a = str(12)
+# b = str(2.1)
+# print(a, b)
+
+# Setting value to integer:
+
+# a = int("abc")
+# b = int(2.1)
+# print(b)
+
+
+# Setting to value to Float :
+
+# a = float(1)
+# b = float("abc")
+# print(a)
+
+
+# ------------------------------------------EXERCISE--------------------------------------
+
+# 1.What will be the output of the following codes
+# x = 5
+# print(type(x))
+
+# a = 10.5
+# print(type(a))
+
+# b = "Hello World"
+# print(type(b))
+
+# c = ["apple", "banana", "cherry"]
+# print(type(c))
+
+# d = ("apple", "banana", "cherry")
+# print(type(d))
+
+# e = {"name": "John", "age": 36}
+# print(type(e))
+
+# f = True
+# print(type(f))
+
+# 2. Suggest changes in codes to print the sum of two variables:
+
+# a = "10"
+# b = "20"
+# print(int(a)+int(b))
+# print(int(a + b))
