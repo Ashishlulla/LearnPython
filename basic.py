08dad379a58cc38f59c36361bdd8aff94ca96a3a
Ashish Rajesh Lulla <65806448+Ashishlulla@users.noreply.github.com>
2022-01-05 19:23:28 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,65 @@
+# print() function in Python:
+
+# Printing String :
+# print("Hello World")
+
+# Printing Integer:
+# print(5)
+
+# Printing Float Number:
+# print(5.6)
+
+# Printing Addition, Subtraction , Multiplication , Division, Double Division,  Mod of two Number :
+
+# print(20 + 10)  # output 30
+# print(20 - 10)  # output 10
+# print(20 * 10)  # output 200
+# print(20 / 6)  # output 2.0
+# print(20// 6)  # output 2
+# print(20 % 6)  # output 3
+
+# Printing List, tuple, Dictionary, Set
+
+# print([1, 2, 3, 4])   # output [1,2,3,4]
+# print((1, 2, 3, 4))   # output (1,2,3,4)
+# print({1, 2, 2, 3, 3, 3})  # output {1, 2, 3}
+# print({1: "abc", 2: "xyz"})  # output  {1: 'abc', 2: 'xyz'}
+
+
+# -----------------------------INDENTATION(Most Important Concept) ----------------------------
+# No Indention (no spacd or tab)
+#  if 10 == 10:
+#  print(True)
+# else:
+#     print(False)
+
+# indentation (space or tab)
+# if 10 == 10:
+#     print(True)
+# else:
+#     print(False)
+
+# ----------------------------------------COMMENTS-------------------------------------------
+
+# THIS IS SINGLE LINE COMMENT
+
+"""
+    THIS IS MULTILINE COMMENT
+     WITH DOUBLE QUOTES.....
+"""
+'''
+     THIS IS MULTILINE COMMENT
+     WITH SINGLE QUOTES.....
+'''
+
+
+# -------------------------------------- EXERCISE ----------------------------------
+
+# print("Hello World")
+
+#
+# if 5 > 2:
+#  print("Five is greater than two!")
+
+
+
