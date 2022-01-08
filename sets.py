c548cadc92d85b8d96ec86d11cecd6a19037c101
Ashish Rajesh Lulla <65806448+Ashishlulla@users.noreply.github.com>
2022-01-08 23:58:13 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,26 @@
+# Creating a Empty set :
+
+# a = set()
+# print(a)
+# print(type(a))
+
+# Creating valued set:
+
+# a = {1, 2, 3, 4, 5}
+# print(type(a))
+# print(a)
+
+# Adding value in set :
+
+# a = {1, 2, 3, 4, 5}
+# a.add(6)
+# print(a)
+
+# Union , intersection , symmetric_difference
+
+# a = {1, 2, 3, 4, 5, 6}
+# b = {4, 5, 6, 7, 8}
+# print(a.union(b))
+# print(a.intersection(b))
+# print(b.symmetric_difference(a))
+# print(a.difference(b))
