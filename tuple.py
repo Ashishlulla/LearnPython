4746d8f207beade8b46e9f791eff045b08b9c718
Ashish Rajesh Lulla <65806448+Ashishlulla@users.noreply.github.com>
2022-01-08 23:57:23 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,19 @@
+# Creating Empty tuple:
+
+# tpl = ()
+
+# creating tuple with single value:
+
+# tpl = (1,)
+# print(type(tpl))
+
+# Creating tuple with multi value:
+
+# tpl = (1, 2, 3, 4, 5)
+# print(tpl)
+
+# Slicing tuple:
+
+# tpl = (1, 2, 3, 4, 5, 6, 7, 8)
+# # print(tpl[2:5]) #output = (3,4,5)
+# print(tpl[::-1])  # output = (8, 7, 6, 5, 4, 3, 2, 1)
